a83f7e3e0944c16ad3e7ec23e4872cd3da3760d5
Christine Yan <45699601+yanchristine@users.noreply.github.com>
2020-10-13 00:21:21 -0400

    Update main encode method

--- Playfair.java
+++ Playfair.java
@@ -154,11 +154,34 @@ public class Playfair {
         return newVal;
     }
     
+    // Main Encode Function
     public static String encode(String input, String[][] table) {
-        String text = JtoI(input);
-        text = doubles(text);
-        text = addZ(text);
+        String txt = JtoI(input);
+        txt = doubles(txt);
+        txt = addZ(txt);
         String output = "";
+        
+        int len = txt.length() - 1;
+        int i = 0;
+        while (i < len) {
+            String char1 = "" + txt.substring(i, i + 1);
+            String char2 = "" + txt.substring(i + 1, i + 2);
+            String pairVals = char1 + char2;
+
+            int[] pair1 = makePair(char1, table);
+            int[] pair2 = makePair(char2, table);
+            if (pair1[0] == pair2[0]) {
+                pairVals = verticalEncode(pairVals, table);
+            } else {
+                if (pair1[1] == pair2[1]) {
+                    pairVals = horizontalEncode(pairVals, table);
+                } else {
+                    pairVals = regular(pairVals, table);
+                }
+            }
+            output = output + pairVals;
+            i = i + 2;
+        }
         return output;
     }
     
